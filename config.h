073f4612dc93630b1400394f7e76eb3a46267c0b
sterling robison <srobison@goalzero.com>
2022-02-07 22:51:39 -0700

    demo full servo range

--- config.h
+++ config.h
@@ -11,8 +11,5 @@ const int PIN_SENSOR_SDA = D2;    // The GPIO number of the pin where the SDA pi
 const int PIN_SENSOR_SCL = D1;    // The GPIO number of the pin where the SCL pin of the APDS-9660 sensor is connected.
 const uint8_t SENSOR_TRIGGER_THRESHOLD = 100;  // The proximity value level that triggers a lid opening. (lower=brighter, higher=darker)
 
-const int LID_START_POSITION = 90;
-const int LID_END_POSITION = 40;
-const int SWITCH_START_POSITION = 0;
-const int SWITCH_END_POSITION = 145;
-const int SWITCH_HALF_POSITION = 90;
+const int LID_START_POSITION = 0;
+const int LID_END_POSITION = 180;
