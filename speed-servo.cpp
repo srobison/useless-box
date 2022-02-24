f97fb864e702452c62097b901d1b7403386f3d30
Sterling Robison <sterling.robison@gmail.com>
2022-02-23 21:59:25 -0700

    increase sensor threshold to something that isn't always triggered add improved servo code from thingiverse

--- speed-servo.cpp
+++ speed-servo.cpp
@@ -1,41 +1,39 @@
 #include "speed-servo.h"
 
-const int SLOW_MOVE_DELAY = 15;
-const int Fast_MOVE_DELAY = 2;
+const int SLOW_MOVE_STEPS = 1;
+const int Fast_MOVE_STEPS = 5;
 
 void SpeedServo::attach(uint8_t pin) {
-  _servo.attach(pin);
+_servo.attach(pin,544,2570);
 }
 
 // Valid position: 0-180.
 void SpeedServo::moveNowTo(int newPosition) {
-  _lastPosition = newPosition;
-  _servo.write(newPosition);
+_lastPosition = newPosition;
+_servo.write(newPosition);
 }
 
 // Valid position: 0-180.
 void SpeedServo::moveFastTo(int newPosition) {
-  _moveTo(newPosition, Fast_MOVE_DELAY);
+_moveTo(newPosition, Fast_MOVE_STEPS);
 }
 
 // Valid position: 0-180.
 void SpeedServo::moveSlowTo(int newPosition) {
-  _moveTo(newPosition, SLOW_MOVE_DELAY);
+_moveTo(newPosition, SLOW_MOVE_STEPS);
 }
 
 // Valid position: 0-180.
 void SpeedServo::_moveTo(int newPosition, unsigned long stepDelay) {
-  if(newPosition > _lastPosition) {
-    for (int pos = _lastPosition; pos <= newPosition; pos++) {
-      _servo.write(pos);
-      delay(stepDelay);
-    }
-  } else {
-    for (int pos = _lastPosition; pos >= newPosition; pos--) {
-      _servo.write(pos);
-      delay(stepDelay);
-    }
-  }
+if(newPosition > _lastPosition) {
+for (int pos = _lastPosition; pos <= newPosition; pos = pos + stepDelay) {
+_servo.write(pos);
+}
+} else {
+for (int pos = _lastPosition; pos >= newPosition; pos = pos - stepDelay) {
+_servo.write(pos);
+}
+}
 
-  _lastPosition = newPosition;
+_lastPosition = newPosition;
 }
